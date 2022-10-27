1011f62eb8239e819f5da8b2040e2e9a9c5447f4
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-27 10:36:11 +0800

    FileChannel write data from buffer

---
+++ src/com/byhuang/channel/FileChannelReadDemo.java
@@ -0,0 +1,33 @@
+package com.byhuang.channel;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2022/10/26 20:49
+ * @description TODO
+ */
+public class FileChannelReadDemo {
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("G:\\testFile\\test01.txt", "rw");
+        FileChannel channel = randomAccessFile.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+        int read = channel.read(byteBuffer);
+        while (read != -1) {
+            System.out.println("read: " + read);
+            byteBuffer.flip();
+            while (byteBuffer.hasRemaining()) {
+                System.out.print((char) byteBuffer.get());
+            }
+            byteBuffer.clear();
+            read = channel.read(byteBuffer);
+        }
+        randomAccessFile.close();
+        System.out.println("\n" + "finish...");
+    }
+}
--- src/com/byhuang/channel/FileChannelWriteDemo.java
+++ src/com/byhuang/channel/FileChannelWriteDemo.java
@@ -1,33 +1,31 @@
 package com.byhuang.channel;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
  * @author mskj-huangbingyi
  * @version 1.0
- * @date 2022/10/26 20:49
+ * @date 2022/10/27 10:08
  * @description TODO
  */
-public class FileChannelDemo {
+public class FileChannelWriteDemo {
 
     public static void main(String[] args) throws IOException {
         RandomAccessFile randomAccessFile = new RandomAccessFile("G:\\testFile\\test01.txt", "rw");
         FileChannel channel = randomAccessFile.getChannel();
         ByteBuffer byteBuffer = ByteBuffer.allocate(48);
-        int read = channel.read(byteBuffer);
-        while (read != -1) {
-            System.out.println("read: " + read);
-            byteBuffer.flip();
-            while (byteBuffer.hasRemaining()) {
-                System.out.print((char) byteBuffer.get());
-            }
-            byteBuffer.clear();
-            read = channel.read(byteBuffer);
+        String data = "aaa";
+        byteBuffer.clear();
+        byteBuffer.put(data.getBytes());
+        byteBuffer.flip();
+        while (byteBuffer.hasRemaining()) {
+            channel.write(byteBuffer);
         }
-        randomAccessFile.close();
-        System.out.println("\n" + "finish...");
+        channel.close();
     }
 }
